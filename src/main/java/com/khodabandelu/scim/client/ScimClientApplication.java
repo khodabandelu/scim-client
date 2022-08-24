f2c88982684219b632e335acccbe32b53ab52c33
Mahdi Khodabandelu <ma.khodabandelu@gmail.com>
2022-08-25 02:47:49 +0430

    initial project

---
+++ src/main/java/com/khodabandelu/scim/client/ScimClientApplication.java
@@ -0,0 +1,13 @@
+package com.khodabandelu.scim.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScimClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScimClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khodabandelu/scim/client/ScimClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khodabandelu.scim.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScimClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

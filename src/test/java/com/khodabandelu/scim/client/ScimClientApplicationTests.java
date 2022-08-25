fd9bfcdcfb712aaf0efecbaa8fe5614835817ac0
Mahdi Khodabandelu <ma.khodabandelu@gmail.com>
2022-08-25 05:52:37 +0430

    add docker file and docker compose

--- src/test/java/com/khodabandelu/scim/client/ScimClientApplicationTests.java
+++ src/test/java/com/khodabandelu/scim/client/ScimClientApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ScimClientApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
